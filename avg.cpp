121877980066bdf83a40db12d3552db51f8777cf
Seelamharshita <112925404+Seelamharshita@users.noreply.github.com>
2022-09-08 11:04:28 +0530

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int sumn=0,sump=0,n=0,p=0;
+	int num;
+	float pavg=0,navg=0;
+	scanf("%d",&num);
+	while(num!=-1)
+	{
+		if(num<0)
+		{
+			sumn=sumn+num;
+		   n=n+1;
+		}
+	   else
+	   {
+	   	sump=sump+num;
+	   p=p+1;
+	   }
+	  scanf("%d"&num);
+	}
+	 navg=sum/n;
+	 pavg=sum/(float)p;
+	 printf("avg p=%d\nsump avg=%d",pavg,navg);
+	 
+	
+}
